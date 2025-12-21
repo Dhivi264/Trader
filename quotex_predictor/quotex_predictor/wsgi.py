6d44a64c47ddccf64b059129749160becc874615
Dhivyadharson_A <dhivyadharson1920060@gmail.com>
2025-12-22 00:11:37 +0530

    Update DJANGO_SETTINGS_MODULE path in wsgi.py

--- quotex_predictor/quotex_predictor/wsgi.py
+++ quotex_predictor/quotex_predictor/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quotex_predictor.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'quotex_predictor.quotex_predictor.settings'
+)
 application = get_wsgi_application()
